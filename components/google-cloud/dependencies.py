b2376554549e6088041898f2921141023b6a5fce
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-07-25 16:49:24 -0600

    modify gcpc dependencies

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -1,4 +1,3 @@
-
 # Copyright 2021 The Kubeflow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Setup configuration of  Google Cloud Pipeline Components client side libraries."""
+"""Setup configuration of  Google Cloud Pipeline Components client side
+libraries."""
 
 
 def make_required_install_packages():
@@ -20,8 +20,8 @@ def make_required_install_packages():
         # Pin google-api-core version for the bug fixing in 1.31.5
         # https://github.com/googleapis/python-api-core/releases/tag/v1.31.5
         "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-        "google-cloud-storage>=1.20.0,<2",
-        "kfp>=1.8.9,<2.0.0",
+        "google-cloud-storage<3,>=2.2.1",
+        "kfp>=2.0.0b1",
         "google-cloud-aiplatform>=1.11.0,<2",
         "google-cloud-notebooks>=0.4.0",
     ]
