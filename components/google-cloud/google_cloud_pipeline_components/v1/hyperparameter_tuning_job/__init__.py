f0fe4e16a5e91df9a4f1709b213ce5b86b91dc56
You Quan Chong <yqchong@google.com>
2022-12-28 18:49:15 -0800

    chore(components): Create GCPC v2 beta release
    PiperOrigin-RevId: 498285424

--- components/google-cloud/google_cloud_pipeline_components/v1/hyperparameter_tuning_job/__init__.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/hyperparameter_tuning_job/__init__.py
@@ -1,4 +1,5 @@
-# Copyright 2021 The Kubeflow Authors. All Rights Reserved.
+# pytype: skip-file
+# Copyright 2022 The Kubeflow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,23 +12,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Module for supporting Google Vertex AI Hyperparameter Tuning Job Op."""
+"""Core modules for AI Platform Pipeline Components."""
 
 import os
 
-from .utils import serialize_parameters, serialize_metrics
-try:
-  from kfp.v2.components import load_component_from_file
-except ImportError:
-  from kfp.components import load_component_from_file
+from . import component as hyperparameter_tuning_job_component
+from . import utils as utils
 
 __all__ = [
     'HyperparameterTuningJobRunOp',
+    'GetTrialsOp',
+    'GetBestTrialOp',
+    'GetBestHyperparametersOp',
+    'GetHyperparametersOp',
+    'GetWorkerPoolSpecsOp',
+    'IsMetricBeyondThresholdOp',
     'serialize_parameters',
     'serialize_metrics',
 ]
 
-
-HyperparameterTuningJobRunOp = load_component_from_file(
-    os.path.join(os.path.dirname(__file__), 'component.yaml'))
-
+HyperparameterTuningJobRunOp = hyperparameter_tuning_job_component.hyperparameter_tuning_job
+GetTrialsOp = utils.GetTrialsOp
+GetBestTrialOp = utils.GetBestTrialOp
+GetBestHyperparametersOp = utils.GetBestHyperparametersOp
+GetHyperparametersOp = utils.GetHyperparametersOp
+GetWorkerPoolSpecsOp = utils.GetWorkerPoolSpecsOp
+IsMetricBeyondThresholdOp = utils.IsMetricBeyondThresholdOp
+serialize_parameters = utils.serialize_parameters
+serialize_metrics = utils.serialize_metrics
